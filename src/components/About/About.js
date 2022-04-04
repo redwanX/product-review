bd6ff05c06dd806fc56271864be5442c968b1971
redwanX <redwanistbd2@gmail.com>
2022-04-04 15:26:30 +0600

    About

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,8 +1,44 @@
 import React from 'react'
-
+import headphone from '../../assets/images/headphone.png'
 const About = () => {
   return (
-    <div>About</div>
+   <div className='w-5/6 mx-auto '>
+     <div className='w-full flex justify-center'>
+          <img className='w-1/4' src={headphone} alt="" />
+        </div>
+      <div className='grid gap-10 grid-cols-1 lg:grid-cols-2 h-full'>
+      <div className=' shadow-lg p-5 rounded'>
+        <h1 className=' text-teal-600 text-2xl font-bold '>Designed for listening freedom</h1>
+        <p className=' text-gray-600 '>A sleek, streamlined, and foldable design, plus seamless setup via the Apple W1 chip, means these are durable and portable enough to stay on-the-move with you.</p>
+      </div>
+      <div className=' shadow-lg p-5 rounded'>
+        <h1 className=' text-teal-600 text-2xl font-bold '>Up to 40 hours of listening time</h1>
+        <p className=' text-gray-600 '>With this much battery life, it’s the perfect everyday headphone, and on-ear cushion cups are made for hours of wear.</p>
+      </div>
+      <div className=' shadow-lg p-5 rounded'>
+        <h1 className=' text-teal-600 text-2xl font-bold '>Power up with Fast Fuel</h1>
+        <p className=' text-gray-600 '>To get you back on track, use 5-minute Fast Fuel for 3 hours of playback when battery is low.</p>
+      </div>
+      <div className='shadow-lg p-5 rounded'> 
+        <h1 className=' text-teal-600 text-2xl font-bold '>Take calls and take control</h1>
+        <p className=' text-gray-600 '>On-ear controls with dual beam-forming mics allow you to take calls, play music, adjust volume, and voice activate wherever your day takes you.</p>
+      </div>
+      
+    </div>
+    <div>
+    <h1 className=' text-center py-5 text-teal-600 text-2xl font-bold '>Key Features</h1>
+    <ul className=' mb-5 list-decimal text-gray-600 font-bold shadow-lg rounded p-10 grid lg:grid-cols-2 '>
+      <li>Bluetooth Wireless Pairing</li>
+      <li>Apple W1 Chip for Quick Pairing</li>
+      <li>Simultaneous Pairing with iOS Devices</li>
+      <li>Clear, Balanced Sound & Noise Isolation</li>
+      <li>On-Board Music, Call & Siri Control</li>
+      <li>Built-In Rechargeable Battery</li>
+      <li>40-Hour Battery Life</li>
+      <li>5-Minute Charge for 3 Hours of Play</li>
+    </ul>
+    </div>
+   </div>
   )
 }
 
--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import headphone from '../../assets/images/headphone.png';
+import { useNavigate } from "react-router-dom";
 const Banner = () => {
+  let navigate = useNavigate();
   return (
     <div className='w-5/6 mx-auto flex justify-center items-center flex-col-reverse lg:flex-row'>
         <div className='flex justify-center'>
@@ -8,7 +10,10 @@ const Banner = () => {
            <h1 className=' font-bold lg:text-8xl md:text-6xl text-3xl '>BEATS SOLO 3</h1>
             <h1 className='text-teal-600 font-bold lg:text-8xl md:text-6xl text-3xl'>WIRELESS</h1>
             <p className=' pt-5 text-justify text-md font-bold'>Beats Solo3 Wireless on-ear headphones immerse you in rich, award-winning sound, everywhere you want to go,Designed for listening freedom,Up to 40 hours of listening time,Power up with Fast Fuel, Take calls and take control.</p>
-           </div>
+            <div className='flex justify-center my-5'>     
+            <button className='hover:bg-teal-600 bg-gray-600 text-white font-bold py-3 px-10 rounded-full shadow-sm transition ease-in-out duration-300' onClick={()=>navigate('/about')}>Full Spec</button> 
+        </div>
+          </div>
         </div>
         <div className='w-full flex justify-center'>
           <img className='w-1/2' src={headphone} alt="" />
