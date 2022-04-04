2c10cc518a5b72da8c78bd968a1eac4550caa307
redwanX <redwanistbd2@gmail.com>
2022-04-04 16:00:28 +0600

    responsive header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import  CustomLink  from '../CustomLink/CustomLink';
-
+import { MenuIcon, XIcon } from '@heroicons/react/solid'
 const Header = () => {
+  const [open,setOpen]=useState(false);
   return (
-    <nav className='flex justify-center shadow-md py-5 mb-5 text-teal-600'>
-      <div className=' lg:w-2/5 w-full flex justify-between flex-col lg:flex-row  text-center font-bold'>
+    <div>
+      <div onClick={()=>setOpen(!open)} className="lg:hidden h-10 w-10 text-teal-600">
+        {open? <XIcon></XIcon>:<MenuIcon></MenuIcon>}
+      </div>
+      <nav className='flex justify-center shadow-md mb-5 text-teal-600'>
+      <div className= {`lg:w-2/5 w-full  lg:flex lg:justify-between lg:flex-row py-5 text-center font-bold ${open?'block':'hidden'}`} >
       <CustomLink className=' hover:text-gray-600 transition ease-in duration-300' to='/'>HOME</CustomLink>
       <CustomLink className='hover:text-gray-600 transition ease-in duration-300' to='/reviews'>REVIEWS</CustomLink>
       <CustomLink className='hover:text-gray-600 transition ease-in duration-300' to='/dashboard'>DASHBOARD</CustomLink>
@@ -12,6 +17,7 @@ const Header = () => {
       <CustomLink className='hover:text-gray-600 transition ease-in duration-300' to='/about'>ABOUT</CustomLink>
       </div>
     </nav>
+    </div>
   )
 }
 
--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -4,7 +4,7 @@ const PageNotFound = () => {
   return (
     <div className='flex justify-center items-center flex-col'>
             <img  className='w-1/2' src={notFound} alt="" />
-            <h3 className='py-5 font-bold text-2xl text-blue-600'>Sorry! Page Not Found</h3>
+            <h3 className='py-5 font-bold text-2xl text-teal-600'>Sorry! Page Not Found</h3>
     </div>
   )
 }
